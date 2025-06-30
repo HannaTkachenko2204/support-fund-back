6fac1615cf993939596d9252253b6f317daea17d
Анна <anna.kuvakina91@gmail.com>
2025-06-30 18:47:05 +0300

    render

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,11 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = new Pool({
-  host: process.env.PGHOST,
-  port: Number(process.env.PGPORT),
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect()
--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['https://support-fund-front.vercel.app'],
   credentials: true,
 }));
 app.use(express.json());
